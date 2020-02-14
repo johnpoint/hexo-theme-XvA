6fa5885555c6dc03ebe5ea8456307278223cf263
johnpoint <me@lvcshu.com>
2020-02-14 11:42:06 +0800

    fix

--- source/js/night.js
+++ source/js/night.js
@@ -1,39 +1,40 @@
-var c = document.cookie.split(';');
+var c = document.cookie.split(";");
 for (let index = 0; index < c.length; index++) {
-    c[index] = c[index].split('=')
-};
+  c[index] = c[index].split("=");
+}
 var darkmode = "";
 for (let index = 0; index < c.length; index++) {
-    if (c[index][0].replace(' ', '') == "darkmode") {
-        darkmode = c[index][1].replace(' ', '');
-    }
-};
+  if (c[index][0].replace(" ", "") == "darkmode") {
+    darkmode = c[index][1].replace(" ", "");
+  }
+}
 function daynight() {
-    if (darkmode == "day") {
-        darkmode = "night";
-        document.cookie = "darkmode=night;path=/;";
-        $('body').addClass('night');
-        $('html').addClass('night');
-        $('.icon-moon')[0].style.display = "none";
-        $('.icon-Sun')[0].style.display = "";
-    } else {
-        darkmode = "day";
-        document.cookie = "darkmode=day;path=/;";
-        $('body').removeClass('night');
-        $('html').removeClass('night');
-        $('.icon-moon')[0].style.display = "";
-        $('.icon-Sun')[0].style.display = "none";
-    }
-};
-if (darkmode == "") {
+  if (darkmode == "day") {
+    darkmode = "night";
+    document.cookie = "darkmode=night;path=/;";
+    $("body").addClass("night");
+    $("html").addClass("night");
+    $(".icon-moon")[0].style.display = "none";
+    $(".icon-Sun")[0].style.display = "";
+  } else {
     darkmode = "day";
-    $('.icon-moon')[0].style.display = "none";
+    document.cookie = "darkmode=day;path=/;";
+    $("body").removeClass("night");
+    $("html").removeClass("night");
+    $(".icon-moon")[0].style.display = "";
+    $(".icon-Sun")[0].style.display = "none";
+  }
+}
+if (darkmode == "") {
+  darkmode = "day";
+  $(".icon-moon")[0].style.display = "";
+  $(".icon-Sun")[0].style.display = "none";
 } else {
-    if (darkmode == "night") {
-        $('body').addClass('night');
-        $('html').addClass('night');
-        $('.icon-moon')[0].style.display = "none";
-    } else {
-        $('.icon-Sun')[0].style.display = "none";
-    }
+  if (darkmode == "night") {
+    $("body").addClass("night");
+    $("html").addClass("night");
+    $(".icon-moon")[0].style.display = "none";
+  } else {
+    $(".icon-Sun")[0].style.display = "none";
+  }
 }
