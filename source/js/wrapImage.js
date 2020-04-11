a89f78a75db5495ab7fcf1b6bcda7eaaa3c802a0
johnpoint <me@lvcshu.com>
2020-04-11 12:22:57 +0800

    压缩 CSS

--- source/js/wrapImage.js
+++ source/js/wrapImage.js
@@ -1,1 +1,30 @@
-$(document).ready(function(){wrapImageWithFancyBox()});function wrapImageWithFancyBox(){$("img").not(".sidebar-image img").not("#author-avatar img").not(".mdl-menu img").not(".something-else-logo img").each(function(){var $image=$(this);var imageCaption=$image.attr("alt");var $imageWrapLink=$image.parent("a");var src=this.getAttribute("src");var idx=src.lastIndexOf("?");if(idx!=-1){src=src.substring(0,idx)}$imageWrapLink=$image.wrap('<a href="'+src+'"></a>').parent("a");$imageWrapLink.attr("data-fancybox","images");if(imageCaption){$imageWrapLink.attr("data-caption",imageCaption)}});$().fancybox({selector:'[data-fancybox="images"]',thumbs:false,hash:true,loop:false,fullScreen:false,slideShow:false,protect:true,})};
+$(document).ready(function() {
+    wrapImageWithFancyBox()
+});
+
+function wrapImageWithFancyBox() {
+    $("img").not(".sidebar-image img").not("#author-avatar img").not(".mdl-menu img").not(".something-else-logo img").each(function() {
+        var $image = $(this);
+        var imageCaption = $image.attr("alt");
+        var $imageWrapLink = $image.parent("a");
+        var src = this.getAttribute("src");
+        var idx = src.lastIndexOf("?");
+        if (idx != -1) {
+            src = src.substring(0, idx)
+        }
+        $imageWrapLink = $image.wrap('<a href="' + src + '"></a>').parent("a");
+        $imageWrapLink.attr("data-fancybox", "images");
+        if (imageCaption) {
+            $imageWrapLink.attr("data-caption", imageCaption)
+        }
+    });
+    $().fancybox({
+        selector: '[data-fancybox="images"]',
+        thumbs: false,
+        hash: true,
+        loop: false,
+        fullScreen: false,
+        slideShow: false,
+        protect: true,
+    })
+};
