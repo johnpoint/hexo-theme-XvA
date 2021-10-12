a68c62ed83eaedf523278d074506b065aef7e9dd
johnpoint <me@lvcshu.com>
2021-10-12 18:09:44 +0800

    fix

--- source/js/disqus-lazyload.js
+++ source/js/disqus-lazyload.js
@@ -4,18 +4,19 @@ function isInViewPortOfOne(el) {
     const offsetTop = el.offsetTop
     const scrollTop = document.documentElement.scrollTop
     const top = offsetTop - scrollTop
-    // 这里有个+100是为了提前加载+ 100
+        // 这里有个+100是为了提前加载+ 100
     return top <= viewPortHeight + 100
 }
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
-var check = async function () {
+var check = async function() {
     while (true) {
-        if (isInViewPortOfOne($('#disqus_thread')[0]) && status == 0) {
+        if (isInViewPortOfOne(document.querySelectorAll('#disqus_thread')[0]) && status == 0) {
             disqusload();
             status = 1;
-            $('#loaddisqus')[0].innerText = "";
+            document.querySelectorAll('#loaddisqus')[0].innerText = "";
             break;
         }
         await sleep(1000);
