3e2f44d240597f28a62856548396095fc1c198bc
johnpoint <me@lvcshu.com>
2022-10-24 16:02:06 +0800

    add: 目录高亮当前章节

--- source/js/totop.js
+++ source/js/totop.js
@@ -4,6 +4,20 @@ const xvaversion = "v3.9.5"
 
 console.log("XvA " + xvaversion)
 
+let toc = [];
+
+$(document).ready(function () {
+    let l = document.querySelectorAll(".post-toc-link");
+    for (i = 0; i < l.length; i++) {
+        title = decodeURIComponent(l[i].hash).replace("#", "", 1);
+        toc.push({
+            id: i,
+            title: title,
+            top: $("#" + title)[0].offsetTop
+        })
+    }
+    toc = toc.reverse();
+});
 
 function totop() {
     cancelAnimationFrame(timer);
@@ -35,7 +49,7 @@ if (judgeIfTop()) {
 }
 
 
-var scroll = function() {
+var scroll = function () {
     if (judgeIfTop() && topstatus == 1) {
         $(".totop")[0].style.display = "none";
         topstatus = 0
@@ -47,15 +61,41 @@ var scroll = function() {
 };
 
 var waiting = false;
-$(window).scroll(function() {
+let nowToc = -1;
+let tocColor = "";
+try {
+    tocColor = document.querySelectorAll(".post-toc-link")[0].style.color || '';
+} catch (e) {
+}
+
+$(window).scroll(function () {
     if (waiting) {
         return;
     }
     waiting = true;
 
+    nowTop = document.documentElement.scrollTop - 200;
+    if (toc.length != 0) {
+        let newToc = toc[toc.length - 1].id || 0;
+        for (let x = 0; x < toc.length; x++) {
+            if (toc[x].top < nowTop) {
+                newToc = toc[x].id;
+                break;
+            }
+        }
+        if (nowToc != newToc) {
+            nowToc = newToc
+            for (let x = 0; x < toc.length; x++) {
+                $(".post-toc-link")[x].style.color = tocColor;
+            }
+            document.querySelectorAll(".post-toc-link")[newToc].style.color = "red";
+        }
+    }
+
+
     scroll();
 
-    setTimeout(function() {
+    setTimeout(function () {
         waiting = false;
-    }, 800);
+    }, 100);
 });
